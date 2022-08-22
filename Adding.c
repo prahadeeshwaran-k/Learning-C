5a8e3b513d924341e7fde895418061fd0b6e3431
prahadeeshwaran-k <prahadeeshwaran.k.2021.ece@ritchennai.edu.in>
2022-08-21 22:20:39 -0700

    Test

--- Adding.c
+++ Adding.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 
 int main(){
-    int a,b,sum;
-    scanf("%d", &a);
-    scanf("%d", &b);
-    sum = a + b;
-    printf("%d\n", sum);
-    return 0;
+    float a,b,ans;
+    ans = 0;
+    char control;
+    scanf("%c", &control);
+    scanf("%f", &a);
+    scanf("%f", &b);
+    switch (control) {
+        case '+':ans = a + b;
+            printf("%f",ans);
+            break;
+        case '-':ans = a - b;
+            printf("%f",ans);
+            break;
+        case '*':ans = a * b;
+            printf("%f",ans);
+            break;
+        case '/':ans = a / b;
+            printf("%f",ans);
+            break;
+        default:
+            printf("No Operation found");
+            break;
+    }
+    return main();
 }
